38a471d2c4be5ff3a801b662f635845c16841f34
azmathias <azramathias@stuperuser.onmicrosoft.com>
2019-11-06 19:04:30 +0000

    Initial commit

---
+++ WordUnscrambler/Constants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordUnscrambler
+{
+    class Constants
+    {
+        public const string EnterTestOption = "Enter scrambled word(s) manually or as a file: F - File, M - Manual:";
+        public const string ContinueProgram = "Would you like to continue? Y/N";
+        
+        public const string EnterTestViaFile = "Enter full path including the file name: ";
+        public const string EnterTestManually = "Enter words manually (separated by commas):";
+        public const string ScrambledWordsNotRecognised = "Option not recognised";
+        
+        public const string ErrorScrambledWordsCannotBeLoaded = "Scrambled words cannot be loaded:";
+        public const string ErrorProgramWillBeTerminated = "The program will be terminated";
+        
+        public const string MatchFound = "Match Found for {0}: {1}";
+        public const string MatchNotFound = "No matches found";
+
+        public const string Yes = "Y";
+        public const string No = "N";
+        public const string File = "F";
+        public const string Manual = "M";
+
+        public const string wordListFileName = "wordlist.txt";
+    }
+}
